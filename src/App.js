35bda08e3a5b60a86232eb2300f726633d3cd964
TinaHeiligers <tina.heiligers@gmail.com>
2017-05-06 20:21:04 -0400

    basic setup done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import CalendarSlider from './CalendarSlider'
 import './App.css';
+import './CalendarSlider.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Calendar Slider</h2>
+          <CalendarSlider />
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
       </div>
     );
